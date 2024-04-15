e292bbda22f58dd8b56d5952d60d828263562f05
Marcos <asesoralanya19@gmail.com>
2024-04-14 21:29:25 -0500

    feat: modify img hero

--- src/pages/home/hero/index.tsx
+++ src/pages/home/hero/index.tsx
@@ -2,7 +2,7 @@ import * as s from "./style";
 
 const Hero = () => {
 	const img =
-		"https://s3-alpha-sig.figma.com/img/32a9/6b23/5dde658f0ce4b45b38e24b2a6ecb2dc6?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=atVa0GLmWg7V30VsbbbKh2CPC-d3lawePZ5fk~tjYxOBdMumUYn-y8plGU~8L9f5ALHaSR0eOqriLdOUWZYVkgua6J8cPt0wTovABY166n8i9nOkV7oU2T9etGKHGUf44bpoMkg6GjH4W74S3syVAItezQcPFRcAQxfhSPqQNUr0qsoIdpypilvQU8RYaRG7hC-DAvOhL1m3ZZd~52yXSzeL2eFK44YftIVDIIUlONXrFwdR1j5UQjFYOR5qL0uQ5gvYmgN36RbaLhplh2mgpJ~sf5sETf-xdYF9zLcqX4LwUzxQRmjtDqRSMuTCMo8wuGeSKuLYVQqVM7xispaMgg__";
+		"https://s3-alpha-sig.figma.com/img/32a9/6b23/5dde658f0ce4b45b38e24b2a6ecb2dc6?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=e-VmWyLWePkyUwjLEmlpTSaFWmfQqfVkketmXRUKDgTDcV~Jh52reTkbgSVFknK~FBrhK6YMHf6k94sooIHsdpTuqeC9vf7WOPwS3PB4T6Aq3McqQX88DIXCx7EpKv7O5YPvfFcDd3x25LFZPsZuRiszDX-QpROVhcYuEzD-gRqk9MzfLaXSSbwfVz-OFAIUBYCT5SeLxHTXRXHdEiqLnhnFl59htD1WOpR~Zofkp2max1eCq0zVHSLyMx9CMWD~rsaBk~iP8AxT5paIPoySHHffWRAvY5QzDwoX3Sf150KcCBu153VaPWLQaYaUSnTFX7mw4hmxdXJb~d-Q-frYDA__";
 
 	return (
 		<s.Wrapper>
