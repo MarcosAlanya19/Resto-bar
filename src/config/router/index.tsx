51874d1971a3e6d7e7e816eb17f3849f132bb5fe
Marcos <asesoralanya19@gmail.com>
2024-05-06 02:09:20 -0500

    feat

--- src/config/router/index.tsx
+++ src/config/router/index.tsx
@@ -1,12 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Dashboard from "../../pages/dashboard";
 import Home from "../../pages/home";
 import Login from "../../pages/login";
 
 export const RoutesConfig = () => (
-  <Routes>
+  <>
     <Route path="/" element={<Home />} />
     <Route path="/login" element={<Login />} />
     <Route path="/dashboard" element={<Dashboard />} />
-  </Routes>
+  </>
 );
