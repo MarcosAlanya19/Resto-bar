49c74843a93178fa4dfc59931d48aa1a3988de8d
Marcos <asesoralanya19@gmail.com>
2024-05-06 02:04:20 -0500

    feat: config routes

--- src/config/router/index.tsx
+++ src/config/router/index.tsx
@@ -1,19 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../../pages/dashboard";
 import Home from "../../pages/home";
 import Login from "../../pages/login";
-import Dashboard from "../../pages/dashboard";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-	{
-		path: "/login",
-    element: <Login />,
-	},
-	{
-		path: "/dashboard",
-    element: <Dashboard />,
-	}
-]);
+export const RoutesConfig = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+  </Routes>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './config/global-css/index.ts';
-import { router } from './config/router/index.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./config/global-css/index.ts";
+import { RoutesConfig } from "./config/router/index.tsx";
 
 const theme = {
-  primary: '#ECA400',
-  secondary: '#F14A41',
-  terceary: '#3FB43D',
-  white: '#FDFFFC',
-  black: '#2F2D2E',
+	primary: "#ECA400",
+	secondary: "#F14A41",
+	terceary: "#3FB43D",
+	white: "#FDFFFC",
+	black: "#2F2D2E",
 };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-			<RouterProvider router={router} />
-    </ThemeProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<BrowserRouter>
+				<RoutesConfig />
+			</BrowserRouter>
+		</ThemeProvider>
+	</React.StrictMode>
 );
