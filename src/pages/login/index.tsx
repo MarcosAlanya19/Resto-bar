ebb9e1876ae2ba0b79eedb585d6d959743dd0da5
Marcos <asesoralanya19@gmail.com>
2024-06-07 15:10:27 -0500

    feat: implement useform in login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,72 +1,21 @@
+import * as s from "./styles";
+
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import styled, { keyframes } from "styled-components";
 import { Text } from "../../components/atomic/text";
-
-const jump = keyframes`
-  from{
-    transform: translateY(0)
-  }
-  to{
-    transform: translateY(-3px)
-  }
-`;
-
-const Wrapper = styled.section`
-	display: flex;
-	flex-direction: column;
-	background-color: #000;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	color: #fff;
-	padding: 64px;
-`;
-
-const Form = styled.form`
-	position: relative;
-
-	width: 100%;
-	padding: 20px 0;
-	display: grid;
-	gap: 16px;
-`;
-
-const Input = styled.input`
-	padding: 11px 13px;
-	background: #f9f9fa;
-	border-radius: 4px;
-	outline: 0;
-	border: 1px solid rgba(245, 245, 245, 0.7);
-	font-size: 14px;
-	transition: all 0.3s ease-out;
-	box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
-	:focus,
-	:hover {
-		box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
-	}
-`;
-
-const Button = styled.button`
-	padding: 11px 13px;
-	color: rgb(253, 249, 243);
-	font-weight: 600;
-	text-transform: uppercase;
-	background: #f03d4e;
-	border: none;
-	border-radius: 3px;
-	outline: 0;
-	cursor: pointer;
-	margin-top: 0.6rem;
-	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
-	transition: all 0.3s ease-out;
-	:hover {
-		background: rgb(200, 50, 70);
-		animation: ${jump} 0.2s ease-out forwards;
-	}
-`;
 
 const Login = () => {
 	const navigate = useNavigate();
+
+	const {
+		formState: { errors },
+		register,
+	} = useForm({
+		defaultValues: {
+			email: "",
+			user_password: "",
+		},
+	});
 
 	return (
 		<div
@@ -87,23 +36,40 @@ const Login = () => {
 				src="./img/login/img-login.jpg"
 				alt="img-login"
 			/>
-			<Wrapper>
+			<s.Wrapper>
 				<Text
 					style={{ textTransform: "uppercase", fontSize: "32px" }}
 					type="title"
 					text="Iniciar Sesión"
 				/>
-				<Form>
-					<Input type="email" name="email" placeholder="example@gmail.com" />
-					<Input type="password" name="password" placeholder="********" />
-				</Form>
-				<Button
+				<s.Form>
+					<s.Input
+						id="email"
+						{...register("email", { required: "Eamil is required" })}
+						type="email"
+						name="email"
+						placeholder="example@gmail.com"
+					/>
+					{errors.email && <p>{errors.email.message}</p>}
+
+					<s.Input
+						id="user_password"
+						{...register("user_password", {
+							required: "user_password is required",
+						})}
+						type="text"
+						name="user_password"
+						placeholder="********"
+					/>
+					{errors.user_password && <p>{errors.user_password.message}</p>}
+				</s.Form>
+				<s.Button
 					style={{ width: "100%" }}
 					onClick={() => navigate("/dashboard/store")}
 				>
 					Entrar
-				</Button>
-			</Wrapper>
+				</s.Button>
+			</s.Wrapper>
 		</div>
 	);
 };
---
+++ src/pages/login/styles.ts
@@ -0,0 +1,64 @@
+import styled, { keyframes } from "styled-components";
+
+export const jump = keyframes`
+  from{
+    transform: translateY(0)
+  }
+  to{
+    transform: translateY(-3px)
+  }
+`;
+
+export const Wrapper = styled.section`
+	display: flex;
+	flex-direction: column;
+	background-color: #000;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	color: #fff;
+	padding: 64px;
+`;
+
+export const Form = styled.form`
+	position: relative;
+
+	width: 100%;
+	padding: 20px 0;
+	display: grid;
+	gap: 16px;
+`;
+
+export const Input = styled.input`
+	padding: 11px 13px;
+	background: #f9f9fa;
+	border-radius: 4px;
+	outline: 0;
+	border: 1px solid rgba(245, 245, 245, 0.7);
+	font-size: 14px;
+	transition: all 0.3s ease-out;
+	box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
+	:focus,
+	:hover {
+		box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
+	}
+`;
+
+export const Button = styled.button`
+	padding: 11px 13px;
+	color: rgb(253, 249, 243);
+	font-weight: 600;
+	text-transform: uppercase;
+	background: #f03d4e;
+	border: none;
+	border-radius: 3px;
+	outline: 0;
+	cursor: pointer;
+	margin-top: 0.6rem;
+	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+	transition: all 0.3s ease-out;
+	:hover {
+		background: rgb(200, 50, 70);
+		animation: ${jump} 0.2s ease-out forwards;
+	}
+`;
