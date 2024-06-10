978b1c2f376cbed5ebae7c9ed516a5cdd84fd966
Marcos <asesoralanya19@gmail.com>
2024-06-10 01:14:56 -0500

    feat: complemete card in process

--- src/pages/dashboard/pages/reception/components/cards/card-order-process/index.tsx
+++ src/pages/dashboard/pages/reception/components/cards/card-order-process/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import * as s from "./styles";
 
 import { Text } from "../../../../../../../components/atomic/text";
 import { apiConfig } from "../../../../../../../config/axios";
 import { IOrder, IOrderStatus } from "../../../../../../../types/order.type";
+import dayjs from "dayjs";
+import { toast } from "react-toastify";
 
 interface IProps {
 	data: IOrder;
@@ -21,6 +24,7 @@ export const CardOrderProcess: React.FC<IProps> = (props) => {
 			await apiConfig.patch(`/orders/${orderId}`, {
 				newStatus: IOrderStatus.delivered,
 			});
+			toast.success(`Se paso a entregado la orden: ${props.data.order_id}`)
 			props.refresh();
 		} catch (error) {
 			console.error("Error updating order status:", error);
@@ -46,44 +50,54 @@ export const CardOrderProcess: React.FC<IProps> = (props) => {
 				(e.currentTarget as HTMLDivElement).style.transform = "scale(1)";
 			}}
 		>
-			<div>
-				<Text
-					text={`Cliente: ${order.user_name}`}
-					type="textDefault"
-					style={{ fontWeight: "bold", marginBottom: "8px" }}
-				/>
-				<Text
-					text={`Fecha: ${new Date(order.order_date).toLocaleString()}`}
-					type="textDefault"
-					style={{ marginBottom: "8px", color: "#757575" }}
-				/>
-				<ul style={{ listStyleType: "none", paddingLeft: 0 }}>
-					{order.items.map((item) => (
-						<li key={item.id} style={{ marginBottom: "4px" }}>
-							<Text
-								text={`${item.item_name} - Cantidad: ${item.quantity}`}
-								type="textDefault"
-								style={{ color: "#424242" }}
-							/>
-						</li>
-					))}
-				</ul>
+			<div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+				<div style={{ display: "grid", gap: "4px" }}>
+					<div>
+						<Text
+							text={`Local encargado:`}
+							type="textDefault"
+							weight="medium"
+						/>
+						<Text text={` ${order.store.store_name}`} type="textDefault" />
+					</div>
+					<div>
+						<Text
+							text={`Cliente: ${order.user.user_name}`}
+							type="textDefault"
+							weight="medium"
+						/>
+						<div style={{ display: "flex", gap: "4px" }}>
+							<Text text={`${order.user.phone_number}`} type="text" />
+							<Text text={`-`} type="text" />
+							<Text text={`${order.user.address}`} type="text" />
+						</div>
+						<Text
+							text={`Hora pedido: ${dayjs(order.order_date).format("hh:mm A")}`}
+							type="text"
+							style={{ color: "#757575" }}
+						/>
+					</div>
+				</div>
+
+				<div>
+					<ul style={{ listStyleType: "none", paddingLeft: 0 }}>
+						{order.items.map((item) => (
+							<li key={item.id} style={{ marginBottom: "4px" }}>
+								<Text
+									text={`${item.item_name} - Cantidad: ${item.quantity}`}
+									type="textDefault"
+									style={{ color: "#424242" }}
+								/>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
+
 			<div style={{ display: "grid", width: "100%" }}>
-				<button
-					onClick={() => updateOrderStatus(order.order_id)}
-					style={{
-						backgroundColor: "#007BFF",
-						color: "white",
-						border: "none",
-						padding: "8px 16px",
-						borderRadius: "4px",
-						cursor: "pointer",
-						marginRight: "8px",
-					}}
-				>
+				<s.BtnStatus onClick={() => updateOrderStatus(order.order_id)}>
 					Marcar como Entregado
-				</button>
+				</s.BtnStatus>
 			</div>
 		</div>
 	);
---
+++ src/pages/dashboard/pages/reception/components/cards/card-order-process/styles.ts
@@ -0,0 +1,31 @@
+import styled, { keyframes } from "styled-components";
+
+const pulseAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+export const BtnStatus = styled.button`
+	background-color: #007bff;
+	color: white;
+	border: none;
+	padding: 8px 16px;
+	border-radius: 4px;
+	cursor: pointer;
+	margin-right: 8px;
+
+	&:hover {
+		background-color: #0056b3;
+	}
+
+	&:active {
+		animation: ${pulseAnimation} 0.3s ease-out;
+	}
+`;
